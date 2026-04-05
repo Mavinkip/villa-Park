6ad27e3fe3bb1136b50748b2d0b1fc294353956a
Mavinkip <mavinngetich1@gmail.com>
2026-04-05 10:51:39 -0400

    main activity error

--- composeApp/src/commonMain/kotlin/com/villapark/app/MainActivity.kt
+++ composeApp/src/commonMain/kotlin/com/villapark/app/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         startKoin {
             androidContext(this@MainActivity)
+
             modules(appModules)
         }
         setContent {
